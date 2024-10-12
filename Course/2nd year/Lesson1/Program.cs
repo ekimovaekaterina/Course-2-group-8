c34dd6c47b81d3fd7c5916ba78af6de668fbc1d3
ekimovaekaterina <ekimova.ekaterina08@mail.ru>
2024-10-12 17:36:25 +0300

    Добавила решение в Lesson 1

---
+++ Course/2nd year/Lesson1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Практика А
+Console.WriteLine("Практика А");
+int number = 10;
+Console.WriteLine(number);
+double num = 5.5;
+Console.WriteLine(num);
+string first = "Hello, world!";
+Console.WriteLine(first);
+bool isTrue = true;
+Console.WriteLine(isTrue);
+
+//Практика B
+Console.WriteLine("Практика B");
+Console.WriteLine("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write(number1 + number2);
+Console.Write(number1 - number2);
+Console.Write(number1 * number2);
+Console.Write(number1 / number2);
+
+
+//Пракика C
+Console.WriteLine("Практика C");
+Console.Write(int.Parse(Console.ReadLine())+5);
