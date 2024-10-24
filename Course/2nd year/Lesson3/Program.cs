518685fb78c6e32a0e823aaed01d8fb8cbdfbbf5
ekimovaekaterina <ekimova.ekaterina08@mail.ru>
2024-10-24 18:46:21 +0300

    Добавила решение в Lesson3

---
+++ Course/2nd year/Lesson3/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program{
+static void Main(string[] args){
+    //Практика А
+    int[] nums = new int[5];
+    string[] strarr = {"бу", "испугался", "не бойся"};
+    //Практика B
+    int[] numarr = {1, 5, 7, 8};
+    Console.Write(numarr[1]);
+    numarr[2] = 100;
+    Console.WriteLine();
+    Console.WriteLine(numarr.Length);
+}}
